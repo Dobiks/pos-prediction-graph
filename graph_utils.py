ea2304ca56f2a431af6c70e7d41835f05ca47c70
Dobiks <dobiks99@gmail.com>
2023-03-22 17:51:57 +0100

    update

--- graph_utils.py
+++ graph_utils.py
@@ -9,8 +9,10 @@ def plot_graph(G):
     pos = nx.get_node_attributes(G, 'pos')
     x, y, z = zip(*[pos[v] for v in G.nodes()])
 
+    node_names = list(G.nodes())
     # Create a scatter plot for the nodes
-    node_scatter = go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=10, color='red'))
+    node_scatter = go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(
+        size=10, color='red'), text=node_names)
 
     # Create a scatter plot for the edges
     edge_x, edge_y, edge_z = [], [], []
@@ -20,12 +22,18 @@ def plot_graph(G):
         edge_x.extend([x1, x2, None])
         edge_y.extend([y1, y2, None])
         edge_z.extend([z1, z2, None])
-    edge_scatter = go.Scatter3d(x=edge_x, y=edge_y, z=edge_z, mode='lines', line=dict(width=1, color='blue'))
+    edge_scatter = go.Scatter3d(
+        x=edge_x, y=edge_y, z=edge_z, mode='lines', line=dict(width=2, color='blue'))
 
     # Create a subplot with 3D scatter plots for nodes and edges
     fig = sp.make_subplots(rows=1, cols=1, specs=[[{'type': 'scatter'}]])
     fig.add_trace(node_scatter)
     fig.add_trace(edge_scatter)
+
+    # Set the axis limits
+    fig.update_layout(scene=dict(xaxis=dict(range=[-100, 100], autorange=False),
+                                 yaxis=dict(range=[-100, 100], autorange=False),
+                                 zaxis=dict(range=[-230, 10], autorange=False)),)
 
     fig.show()
 
@@ -34,14 +42,14 @@ def random_point_on_sphere(r):
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     z = random.uniform(-1, 0)
-    
+
     x *= r
     y *= r
     z *= r
-    
+
     normalization_factor = math.sqrt(x**2 + y**2 + z**2)
     x /= normalization_factor
     y /= normalization_factor
     z /= normalization_factor
-    
+
     return x, y, z
--- wire_graph.py
+++ wire_graph.py
@@ -3,12 +3,16 @@ from graph_utils import plot_graph
 import pandas as pd
 
 static = pd.read_csv('corrected_static.csv')
-
 G = nx.Graph()
 
 for i in range(len(static)):
     G.add_node(static['name'][i], pos=(static['x'][i], static['z'][i], static['y'][i]))
 
-    
+#add parent basing on static['parent']
+for i in range(len(static)):
+    if static['parent'][i] != '0':
+        G.add_edge(static['name'][i], static['parent'][i])
+
 
 plot_graph(G)
+
